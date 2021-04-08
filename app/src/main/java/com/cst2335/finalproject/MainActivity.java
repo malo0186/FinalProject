6597b7d4f9a9480c3be0b6b167148f7d88addb16
Hamzeh-byte <kadd0024@algonquinlive.com>
2021-04-08 13:45:28 -0400

    Covid 19 Application Changes.

--- app/src/main/java/com/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/com/cst2335/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_covid19 = (ImageButton)findViewById(R.id.btn_covid19);
+        btn_covid19.setOnClickListener(bt -> {
+            Intent nextPage = new Intent(MainActivity.this, CovidMain.class);
+            startActivity(nextPage);
+        });
+
+
+
+//        ImageButton (yourDrawableimage) = (ImageButton)findViewById(R.id.btn_covid19);
+//        btn_covid19.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, YourClass.class);
+//            startActivity(nextPage);
+//        });
+
+
+        //        ImageButton (yourDrawableimage) = (ImageButton)findViewById(R.id.btn_covid19);
+//        btn_covid19.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, YourClass.class);
+//            startActivity(nextPage);
+//        });
+
+
+//        ImageButton btn_covid19 = (ImageButton)findViewById(R.id.btn_covid19);
+//        btn_covid19.setOnClickListener(bt -> {
+//            Intent nextPage = new Intent(MainActivity.this, CovidMain.class);
+//            startActivity(nextPage);
+//        });
+
+
+
     }
 }
